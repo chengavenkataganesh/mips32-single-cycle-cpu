ce1a8cab8ed847eda126695112bac95a90eb93b8
chengavenkataganesh <c.v.ganesh.github@gmail.com>
2025-11-02 12:58:21 +0530

    Update register_file.v

--- src/register_file.v
+++ src/register_file.v
@@ -44,12 +44,6 @@ always @(*) begin
 end
 endmodule
 
-//SIGN EXTENSION
-module sign_xtend(imm_data, xtended);
-input [15:0] imm_data;
-output [31:0] xtended;
-assign xtended = {{16{imm_data[15]}},imm_data};
-endmodule
 
 
 
