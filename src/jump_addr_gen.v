0e81ab322897042f923cef71ebce1d6d8b9815c2
chengavenkataganesh <c.v.ganesh.github@gmail.com>
2025-11-02 12:49:39 +0530

    Create jump_addr_gen.v

---
+++ src/jump_addr_gen.v
@@ -0,0 +1,9 @@
+//SHIFT LEFT 2 JUMP(previously but modified by c.v.ganesh)
+//JUMP ADDRESS GENERATOR/CALCULATOR
+module jump_addr_gen(in_sequence, jump_addr, pcplus1_for_jump);
+input [25:0] in_sequence;
+input [31:0] pcplus1_for_jump;
+output [31:0] jump_addr;
+//assign shifted_out = {pcplus4_for_jump[31:28], in_sequence, 2'b00}; 
+assign jump_addr = {pcplus1_for_jump[31:26],in_sequence};
+endmodule
